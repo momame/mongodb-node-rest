8c5a5c00f4eda7ba256a585deff08d0c1ddbdf58
mo <mehrpak.ie@gmail.com>
2017-08-09 11:38:25 -0700

    defining mongoose connection

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var mongoose = require('mongoose');
+var Location = require('./Location.model');
+
+var port = 8080;
+var db = 'mongodb://localhost/example'
+
+mongoose.connect(db);
