d3f11ac1bd85a3f61a9e85a2788f1fbfeff014ea
mo <mehrpak.ie@gmail.com>
2017-08-09 11:36:46 -0700

    call the packages we need

---
+++ app.js
@@ -0,0 +1,9 @@
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
